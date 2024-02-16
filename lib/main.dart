39a8715fe7ee62a9cde8fedb65fe37f60c81f050
Yasith_99 <94992009+Yasithsarada@users.noreply.github.com>
2024-02-16 23:06:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:online_shopping_store/screens/home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
+////
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
