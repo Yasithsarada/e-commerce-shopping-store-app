46cc914dee8b2724e77694dc08d96cbe9fc183be
Yasith_99 <yasithsarada79@gmail.com>
2024-07-17 22:49:57 +0530

    product diplay page updating day 3 low work

---
+++ lib/screens/add_product_screen_stepper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddProductStepper extends StatefulWidget {
+  const AddProductStepper({super.key});
+
+  @override
+  State<AddProductStepper> createState() => _AddProductStepperState();
+}
+
+class _AddProductStepperState extends State<AddProductStepper> {
+   int currentStep = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+
+  
+} 
